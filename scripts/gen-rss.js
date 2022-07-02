904d3e50b1e8fe0c112b4500c56550664b88db5d
Vahid Al <thevahidal@gmail.com>
2022-07-03 02:43:36 +0430

    issue with directory posts fixed

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -15,20 +15,24 @@ async function generate() {
   await Promise.all(
     posts.map(async (name) => {
       if (name.startsWith('index.')) return
+      try {
+        const content = await fs.readFile(
+          path.join(__dirname, '..', 'pages', 'posts', name)
+        )
 
-      const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
-      )
-      const frontmatter = matter(content)
+        const frontmatter = matter(content)
 
-      feed.item({
-        title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
-        date: frontmatter.data.date,
-        description: frontmatter.data.description,
-        categories: frontmatter.data.tag.split(', '),
-        author: frontmatter.data.author
-      })
+        feed.item({
+          title: frontmatter.data.title,
+          url: '/posts/' + name.replace(/\.mdx?/, ''),
+          date: frontmatter.data.date,
+          description: frontmatter.data.description,
+          categories: frontmatter.data.tag.split(', '),
+          author: frontmatter.data.author
+        })
+      } catch (e) {
+        console.error(e)
+      }
     })
   )
 
